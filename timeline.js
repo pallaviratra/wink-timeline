101497fded632b3ef2821f4a369c82d523213b73
Pallavi Ratra <pallaviratra75@gmail.com>
2020-03-02 16:16:22 +0530

    refactor: add filter on entity extraction & add JSON output option

--- timeline.js
+++ timeline.js
@@ -1,47 +1,70 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-console */
 
-var winkNLP = require( '../src/wink-nlp.js' );
-var its = require( '../src/its.js' );
-var nlp = winkNLP( );
-nlp.load( 'en' );
+var winkNLP = require( 'wink-nlp' );
+var model = require( 'wink-eng-lite-model' );
+var its = require( 'wink-nlp/src/its.js' );
+var nlp = winkNLP( model );
 var fs = require( 'fs' );
-var text = fs.readFileSync( 'C:/Users/hp/Desktop/Graype/michelleO.txt', 'utf8' ); // eslint-disable-line no-sync
+var text = fs.readFileSync( 'michelleO.txt', 'utf8' ); // eslint-disable-line no-sync
 var doc = nlp.readDoc( text );
 
 var year;
-var dup = { };
-var timeline = [ ];
-doc.sentences( )
-   .each( ( s, index ) => {
-            s.entities( )
-             .each( ( e ) => {
-               if ( e.out( ).type === '#DATE' ) {
-                 if ( e.tokens( ).out( its.shape ).indexOf( 'dddd' ) >= ( 0 ) ) {
-                    e.tokens( )
-                     .each( ( t ) => {
-                       if ( t.out( its.shape ) === 'dddd' )
-                         year = t.out( );
-                      } );
-                     timeline.push( { text: e.out( ).text, type: e.out( ).type, year: year, sid: index } );
-                  } else {
-                     e.tokens( )
-                      .each( ( t ) => {
-                        timeline.push( { text: t.out( ) + ' ' + year, type: '#DATE', year: year, sid: index } );
-                      } );
-                  }
-                }
-              } );
-            } );
+var dup = {};
+var timeline = [];
+doc.sentences()
+   .each( ( sentence, index ) => {
+     sentence.entities()
+             .filter( ( entity ) => ( entity.out().type === '#DATE' ) )
+             .each( ( entity ) => {
+               let yearIndex = entity.tokens().out( its.shape ).indexOf( 'dddd' );
+               if ( yearIndex >= 0 ) {
+                 year = entity.tokens().itemAt( yearIndex ).out();
+                 timeline.push( { text: entity.out().text, year: year, sid: index } );
+               } else {
+                  timeline.push( { text: entity.out().text + ' ' + year, year: year, sid: index } );
+               }
+             } ); // each( ( entity )...
+    } ); // each( ( sentence, index )...
 
-timeline.sort(  ( a, b ) => ( a.year - b.year ) );
-var t = timeline.filter( ( e ) => {
-                              dup[ e.sid ] = 1 + ( dup[ e.sid ] || 0 );
-                              return dup[ e.sid ] === 1;
-                            } );
-t.forEach( ( e ) => {
-  console.log( '\x1b[0m\x1b[32m' + e.year + '\x1b[37m\x1b[2m' );
-   console.log( doc.sentences( ).itemAt( e.sid ).out( ) );
-   console.log( );
-} );
-// console.log( t );
+timeline.sort( ( a, b ) => ( a.year - b.year ) );
+timeline = timeline.filter( ( date ) => {
+                     dup[ date.sid ] = 1 + ( dup[ date.sid ] || 0 );
+                     return dup[ date.sid ] === 1;
+                   } ); // filter( ( date )...
+// -- wink-nlp code ends here --
+
+// Print Date and corresponding sentence on console
+timeline.forEach( ( date ) => {
+           console.log( '\x1b[0m\x1b[32m' + date.year + '\x1b[37m\x1b[2m' );
+           console.log( doc.sentences().itemAt( date.sid ).out() );
+           console.log();
+        } );
+
+// Markup Date and money entities and return JSON
+// var finalEvents = [];
+// timeline.forEach( ( date ) => {
+//            var event = nlp.readDoc( doc.sentences().itemAt( date.sid ).out() );
+//            event.entities()
+//                 .each( ( entity ) => {
+//                   if ( entity.out().type === '#DATE'|| entity.out().type === '#MONEY' ) {
+//                     entity.markup( '<em>', '</em>' );
+//                   }
+//                 } );
+//            finalEvents.push( {text: event.markedUpText(), year: date.year})
+//         } );
+// console.log(JSON.stringify(finalEvents, null, 2));
+
+// Print with marked entities on console
+// timeline.forEach( ( date ) => {
+//             var event = nlp.readDoc( doc.sentences().itemAt( date.sid ).out() );
+//             event.entities()
+//                  .each( ( entity ) => {
+//                    if ( entity.out( ).type === '#DATE'|| entity.out( ).type === '#MONEY' ) {
+//                       entity.markup( '<em>', '</em>' );
+//                    }
+//                   } );
+//             console.log( '\x1b[0m\x1b[32m' + date.year + '\x1b[37m\x1b[2m' );
+//             console.log( event.markedUpText() );
+//             console.log();
+//             } );
